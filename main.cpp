25674bb7321d6659c232ef6295cdcb6fb0cca98c
LucasEBSkora <skora.lucas@gmail.com>
2022-06-04 20:46:23 -0300

    adds templates and operator overloads

--- main.cpp
+++ main.cpp
@@ -3,115 +3,157 @@
 #include <vector>
 #include <functional>
 
-#define LAMBDA_CALL(f) ([this] { f ();})
-#define LAMBDA_EXPRESSION(e) ([this] {return e;})
+#define LAMBDA_CALL(f) ([this] { f(); })
+#define LAMBDA_EXPRESSION(e) ([this] { return e; })
 typedef std::deque<std::function<void(void)>> NOPQueue;
 
 NOPQueue NotificationQueue;
 
-struct BooleanAttribute {
-  bool value;
+template <typename T>
+class Attribute
+{
+  T value;
   const std::vector<std::function<void(void)>> notifyList;
 
-  BooleanAttribute(bool value, const std::vector<std::function<void(void)>> notifyList) : value{value}, notifyList{notifyList} {}
+public:
+  Attribute(T value, const std::vector<std::function<void(void)>> notifyList) : value{value}, notifyList{notifyList} {}
 
-    void update(bool newValue) {
-    if (value != newValue) {
-      NotificationQueue.push_back([&, this] {this->notify();});
+  T operator = (T newValue) {
+    if (value != newValue)
+    {
+      NotificationQueue.push_back([&, this]
+                                  { this->notify(); });
       value = newValue;
     }
+    return newValue;
   }
-  
-  void notify() {
-      NotificationQueue.insert(NotificationQueue.end(), notifyList.begin(), notifyList.end());
+
+  void notify()
+  {
+    NotificationQueue.insert(NotificationQueue.end(), notifyList.begin(), notifyList.end());
+  }
+
+  operator T()
+  {
+    return value;
   }
 };
 
-struct Premise {
+class Premise
+{
+
   std::function<bool(void)> expression;
   bool value;
   const std::vector<std::function<void(void)>> notifyList;
 
-  Premise(std::function<bool(void)> expression, const std::vector<std::function<void(void)>> notifyList) 
-    : expression{expression}, value{expression()}, notifyList{notifyList} {}
+public:
+  Premise(std::function<bool(void)> expression, const std::vector<std::function<void(void)>> notifyList)
+      : expression{expression}, value{expression()}, notifyList{notifyList} {}
 
-  void update() {
+  void update()
+  {
     bool newValue = expression();
-    if (value != newValue) {
-      NotificationQueue.push_back([&, this] {this->notify();});
+    if (value != newValue)
+    {
+      NotificationQueue.push_back([&, this]
+                                  { this->notify(); });
       value = newValue;
     }
   }
-  
-  void notify() {
-      NotificationQueue.insert(NotificationQueue.end(), notifyList.begin(), notifyList.end());
+
+  void notify()
+  {
+    NotificationQueue.insert(NotificationQueue.end(), notifyList.begin(), notifyList.end());
+  }
+
+  inline operator bool()
+  {
+    return value;
   }
 };
 
-struct Condition {
+class Condition
+{
   std::function<bool(void)> expression;
   bool value;
   const std::vector<std::function<void(void)>> notifyList;
 
-  Condition(std::function<bool(void)> expression, const std::vector<std::function<void(void)>> notifyList) 
-    : expression{expression}, value{expression()}, notifyList{notifyList} {}
+public:
+  Condition(std::function<bool(void)> expression, const std::vector<std::function<void(void)>> notifyList)
+      : expression{expression}, value{expression()}, notifyList{notifyList} {}
 
-  void update() {
+  void update()
+  {
     bool newValue = expression();
-    if (value != newValue) {
-      NotificationQueue.push_back([&, this] {this->notify();});
+    if (value != newValue)
+    {
+      NotificationQueue.push_back([&, this]
+                                  { this->notify(); });
       value = newValue;
     }
   }
-  
-  void notify() {
-    if (value) {
+
+  void notify()
+  {
+    if (value)
+    {
       NotificationQueue.insert(NotificationQueue.end(), notifyList.begin(), notifyList.end());
     }
   }
+
+  inline operator bool()
+  {
+    return value;
+  }
 };
 
+struct Main
+{
 
-
-struct Main {
-
-  BooleanAttribute atStatus;
+  Attribute<bool> atStatus;
   Premise prStatus;
   Condition cdStatus;
 
-  void mtChange() {
+  void mtChange()
+  {
     std::cout << "hello world!" << std::endl;
-    atStatus.update(true);
+    atStatus = true;
   }
 
-  void NotifyinChange() {
-    NotificationQueue.push_back([&, this] {this->mtChange();});
+  void NotifyinChange()
+  {
+    NotificationQueue.push_back([&, this]
+                                { this->mtChange(); });
   }
 
-  void NotifyAcChange() {
-    NotificationQueue.push_back([&, this] {this->NotifyinChange();});
+  void NotifyAcChange()
+  {
+    NotificationQueue.push_back([&, this]
+                                { this->NotifyinChange(); });
   }
 
-  void NotifyRlChange() {
-    NotificationQueue.push_back([&, this] {this->NotifyAcChange();});
+  void NotifyRlChange()
+  {
+    NotificationQueue.push_back([&, this]
+                                { this->NotifyAcChange(); });
   }
 
-  Main() : 
-    atStatus{false, {LAMBDA_CALL(prStatus.update)}}, 
-    prStatus{LAMBDA_EXPRESSION(atStatus.value == true), {LAMBDA_CALL(cdStatus.update)}}, 
-    cdStatus{LAMBDA_EXPRESSION(prStatus.value), {LAMBDA_CALL(NotifyRlChange)}} {
-    atStatus.update(true);
+  Main() : atStatus{false, {LAMBDA_CALL(prStatus.update)}},
+           prStatus{LAMBDA_EXPRESSION(atStatus == true), {LAMBDA_CALL(cdStatus.update)}},
+           cdStatus{LAMBDA_EXPRESSION(prStatus), {LAMBDA_CALL(NotifyRlChange)}}
+  {
+    atStatus = true;
   }
-
 };
 
 int main()
 {
   Main main;
-  while (!NotificationQueue.empty()) {
+  while (!NotificationQueue.empty())
+  {
     NotificationQueue.front()();
     NotificationQueue.pop_front();
   }
-  
+
   return 0;
 }
